072b520761b7fe5f96a3163a0c6d2e041e6f75b8
UNATHI <keke.ac.za.cput@gmail.com>
2025-05-10 17:41:59 +0200

    Customer POJO Class commited successfully

---
+++ src/main/java/za/co/naturalsip/domain/Customer.java
@@ -0,0 +1,51 @@
+package za.co.naturalsip.domain;
+
+/*Customer.java
+  Customer POJO Class
+  Author: Unathi Keke( 221410848)
+  Date:10 May 2025
+ */
+public class Customer {
+    private Long id;
+    private String name;
+    private String email;
+
+    private Customer(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.email = builder.email;
+    }
+    public static class Builder {
+        private Long id;
+        private String name;
+        private String email;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+        public Customer build() {
+            return new Customer(this);
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
