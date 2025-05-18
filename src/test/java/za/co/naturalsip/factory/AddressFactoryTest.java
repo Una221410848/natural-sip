1e3060772af928ddd79c0ffba587b8110241d0f4
Coben <220235686@mycput.ac.za>
2025-05-18 20:46:56 +0200

    Added AddressFactory and test class

--- src/test/java/za/co/naturalsip/factory/AddressFactoryTest.java
+++ src/test/java/za/co/naturalsip/factory/AddressFactoryTest.java
@@ -2,7 +2,7 @@ package za.co.naturalsip.factory;
 
 /* AddressFactory.java
 Author: Coben Maistry - 220235686
-  Date - 18th May 2025 */
+  Date - 17th May 2025 */
 
 import org.junit.jupiter.api.Test;
 import za.co.naturalsip.domain.Address;
